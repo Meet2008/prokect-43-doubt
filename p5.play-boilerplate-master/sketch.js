669c5a57aa2e4dc4c92c40b3bdb3a1c575d426eb
Meet Ankoliya <s.meet.h.ankoliya@ppsavanicbse.org>
2021-01-16 14:14:10 +0530

    firstCommit

---
+++ p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,40 @@
+/*var hr = hour();
+var mn = minute();
+var sc = second();*/
+
+function setup() {
+  createCanvas(800,400);
+
+
+}
+
+function draw() {
+  background(255,255,255);  
+
+  /*scAngle = map(sc,0,60,0,360);
+  mnAngle = map(mn,0,60,0,360);
+  hrAngle = map(hr,0,60,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,100,0);
+  pop();
+  
+  push();
+  rotate(mnAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,200,0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke(255,0,0);
+  strokeWeight(7);
+  line(0,0,300,0);
+  pop();*/
+
+  
+}
